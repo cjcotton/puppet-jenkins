477ed075c1fe3f2fe5eb379efb52cac757111176
Courtney C <cotton@cottoncourtney.com>
2015-09-27 14:20:50 -0700

    Updating rspect tests: test for included classes.
    x It was missing checking for ::proxy and ::repo

--- spec/classes/jenkins_spec.rb
+++ spec/classes/jenkins_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
   describe 'jenkins' do
-      it { should contain_class 'jenkins::service' }
+    it { should contain_class 'jenkins::service' }
   end 
 
   describe 'jenkins' do
-     it { should contain_class 'jenkins::packages' }
+    it { should contain_class 'jenkins::packages' }
+  end
+
+  describe 'jenkins' do
+    it { should contain_class 'jenkins::repo' } 
+  end
+
+  describe 'jenkins' do 
+    it { should contain_class 'jenkins::proxy' }
   end
